11ddc70e29e4a020294d6713ca0711ed3a7c9b39
Pule <proceedingmc@gmail.com>
2023-11-24 16:39:01 +0200

    0x0A-configuaration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,12 @@
+# Puppet manifest to create a file in /tmp
+
+# Define the file resource
+file { '/tmp/school':
+  ensure  => 'file',       # Ensure that it is a regular file
+  mode    => '0744',        # Set file permissions to 0744
+  owner   => 'www-data',    # Set file owner to www-data
+  group   => 'www-data',    # Set file group to www-data
+  content => 'I love Puppet',  # Set the content of the file
+}
+
+# End of Puppet manifest
