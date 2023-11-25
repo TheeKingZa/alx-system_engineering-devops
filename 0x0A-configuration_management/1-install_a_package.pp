0ec44fc0c1d45337dcead5bd5ddd1eaa697347f4
Pule <proceedingmc@gmail.com>
2023-11-25 05:26:25 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# Puppet manifest to install Flask from pip3
-
-# Define the package resource for Flask
-package { 'Flask':
-  ensure   => '2.1.0',  # Ensure the specific version 2.1.0 is installed
-  provider => 'pip3',   # Use pip3 as the package provider
-  require  => Package['python3-pip'],  # Ensure python3-pip is installed first
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
-
-# End of Puppet manifest
