63605e4979785f35e75b75b8fd3dd624d7f55254
Pule <proceedingmc@gmail.com>
2023-11-25 05:22:12 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,6 +3,7 @@
 # Define the file resource
 file { '/tmp/school':
   ensure  => 'file',       # Ensure that it is a regular file
+  path    => '/tmp/school', # path
   mode    => '0744',        # Set file permissions to 0744
   owner   => 'www-data',    # Set file owner to www-data
   group   => 'www-data',    # Set file group to www-data
