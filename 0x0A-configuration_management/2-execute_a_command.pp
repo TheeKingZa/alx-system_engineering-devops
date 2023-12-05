5794507e480e08f8948a141b1df4a287386a02ae
Pule <proceedingmc@gmail.com>
2023-12-05 16:33:05 +0200

    puppet-lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,8 +2,8 @@
 
 # Define the exec resource to run the pkill command
 exec { 'pkill':
-  command     => 'pkill -9 -f killmenow',  # The pkill command to terminate the process
-  path        => ['/usr/bin', '/usr/sbin', '/bin']       # Set the path for the command
+  command => 'pkill -9 -f killmenow',  # The pkill command to terminate the process
+  path    => ['/usr/bin', '/usr/sbin', '/bin']       # Set the path for the command
 }
 
 # End of Puppet manifest
