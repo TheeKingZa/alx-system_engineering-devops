8251ff881283cb2ad1be584378b6d21760613d3d
Pule <proceedingmc@gmail.com>
2023-12-05 06:50:14 -0800

    edits to 1-install..

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,18 @@
 # 1-install_a_package.pp
 
+# Install a specific version of Werkzeug first
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+}
+
 # Define a package resource for Flask
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip',
+}
+
+# Notify to refresh the shell after the packages are installed
+notify { 'Refresh Shell':
+  subscribe => [Package['Flask'], Package['Werkzeug']],
 }
