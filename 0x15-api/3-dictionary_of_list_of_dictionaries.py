fdd2a5d6246e1125da8566e3b04132f4aae97701
Pule <proceedingmc@gmail.com>
2024-01-03 01:19:02 +0200

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,43 +1,18 @@
 #!/usr/bin/python3
-"""
-This script exports TODO list data for all employees to a JSON file.
-"""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
-
 if __name__ == "__main__":
-    # Fetching all users
-    all_users = requests.get(
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-        'https://jsonplaceholder.typicode.com/users'
-        ).json()
-
-    # Creating dictionary to store data for all employees
-    all_data = {}
-
-    for user in all_users:
-        # Fetching TODO list for each user
-        todo_data = requests.get(
-            'https://jsonplaceholder.typicode.com/todos?userId={}'
-            .format(user['id'])
-        ).json()
-
-        # Creating list of dictionaries for each user
-        user_data_list = [
-            {"username": user['username'],
-             "task": task['title'],
-             "completed": task['completed']
-             } for task in todo_data
-            ]
-
-        # Adding the list to the dictionary with the user ID as the key
-        all_data[user['id']] = user_data_list
-
-    # Creating JSON file
-    json_filename = 'todo_all_employees.json'
-
-    with open(json_filename, 'w') as json_file:
-        json.dump(all_data, json_file)
-
-    print("Data exported to {}".format(json_filename))
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
