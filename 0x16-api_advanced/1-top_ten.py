fa8f8987b95b29cc1921439e7681663e306688ea
Pule <proceedingmc@gmail.com>
2024-01-17 10:39:36 +0200

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 '''
-Module contains a function that makes an API call
+Module contains a function that makes
+an API call
 '''
 import requests
 
 
 def top_ten(subreddit):
     '''
-    Makes an API call to get the top ten hot posts
-    in a given subreddit.
+    Queries the Reddit API and prints the titles
+    of the first 10 hot posts for a given subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit to check.
+        subreddit (str): The name of the subreddit.
 
     Returns:
-        list: List of titles for the top ten hot posts,
-        or an empty list if the subreddit is invalid.
+        None
     '''
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    data = requests.get(url, headers={'User-agent': 'my-bot'})
+    data = requests.get(
+        url, headers={'User-agent': 'my-bot'}, allow_redirects=False
+        )
 
     if data.status_code == 200:
         post_list = data.json().get('data').get('children')
-        titles = [post.get("data").get("title") for post in post_list]
-        return titles
+
+        for post in post_list:
+            print(post.get("data").get("title"))
     else:
-        print("Invalid subreddit or other error.")
-        return []
+        print(None)
