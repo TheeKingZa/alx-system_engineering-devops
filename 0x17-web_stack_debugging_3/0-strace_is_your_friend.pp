0e6a07235e855b2f95c5b68588013b6dffff5284
Pule <proceedingmc@gmail.com>
2024-01-26 10:38:38 +0200

    lint check on new code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,12 @@
 # 0-strace_is_your_friend.pp
+# Fixes a file
 
-# Ensure Apache package is installed
-package { 'apache2':
-  ensure => 'installed',
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure => 'running',
-}
-
-# Your additional resources based on the issue found using strace
-# For example, setting correct file permissions or updating a configuration file
-file { '/path/to/your/file':
-  ensure  => 'file',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => template('your_module/your_template.erb'),
+# Executing a shell command to replace occurrences
+# of ".phpp" with ".php" in the wp-settings.php file
+exec {'replaces wrong php filetype':
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  # Specify the path for the shell command
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
+  # Execute only if the file exists
 }
